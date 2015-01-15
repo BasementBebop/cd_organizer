e4255113ac47c7f1a6a93d9697847349431fbce2
Jessica Hori & Sean Holstrom <student@epicodus.com>
2015-01-15 15:10:23 -0800

    suprise you fantastically

--- app.rb
+++ app.rb
@@ -1,6 +1,23 @@
 require('sinatra')
 require('sinatra/reloader')
-require('cd_organizer')
 require('./lib/cds')
 require('./lib/cd_organizer')
 also_reload('./lib/**/*.rb')
+
+
+get('/') do
+
+to_save = Release.new('artist_name', 'album_name')
+to_save.save()
+
+@list = Release.list_all()
+erb(:form)
+end
+
+post("/artist") do
+  artist_name = params.fetch("artist_name")
+  album_name = params.fetch("album_name")
+  test_cd = Release.new(artist_name, album_name)
+  test_cd.save()
+  erb(:success)
+end
--- spec/cd_organizer_spec.rb
+++ spec/cd_organizer_spec.rb
@@ -3,7 +3,7 @@ require('cd_organizer')
 require('cds')
 
 describe(ArtistLibrary) do
-  describe('#by_artist')
+  describe('#get_releases_lib')
     it("displays all releases by single artist") do
         test_cd = Release.new("summer", "calvin harris")
         test_cd.save()
@@ -12,4 +12,16 @@ describe(ArtistLibrary) do
         test_library = ArtistLibrary.new(test_cd.get_artist(), [test_cd.get_album_name, test_cd2.get_album_name])
       expect(test_library.get_releases_lib()).to(eql(["summer", "outside"]))
     end
+  describe('#get_artist_lib')
+    it("displays artist name of release library") do
+      test_cd = Release.new("summer", "calvin harris")
+      test_cd.save()
+      test_cd2 = Release.new("outside", "calvin harris")
+      test_cd2.save()
+      test_library = ArtistLibrary.new(test_cd.get_artist(), [test_cd.get_album_name, test_cd2.get_album_name])
+      expect(test_library.get_artist_lib()).to(eql("calvin harris"))
+    end
 end
+
+
+#(compiles list of albums into a library, then displays the names of the albums)
